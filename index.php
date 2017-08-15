3c44e0185ba5cc4c8e981cf430d3e8eab10243fd
bronzebautista <bronsbau@gmail.com>
2017-08-15 23:57:05 +0800

    Add files via upload

---
+++ index.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<link href="https://fonts.googleapis.com/css?family=Poppins|Roboto:100,100i,300,300i,400,400i,500&amp;subset=cyrillic,greek,greek-ext" rel="stylesheet">	
+	<link rel="stylesheet" type="text/css" href="css/style.css">
+	<title></title>
+</head>
+<body>
+<header>
+	<img class="logo" src="images/logo.png">
+	<nav>
+		<ul>
+			<li><a href="#">Products</a></li>
+			<li><a href="#">Features</a></li>
+			<li><a href="#">Reviews </a></li>
+			<li><a href="#">Pricing </a></li>
+			<li><a href="#">Contact </a></li>
+		</ul>
+	</nav>
+</header>
+<div id="container">
+		<div class="images">
+			<img class="hero" src="images/hero.jpg">
+		</div>
+		<div class="heading">
+			<h1>Wireless Bluetooth </h1>
+			<h2>Speakers of tomorrow</h2>
+			<p>Beoplay A1 bluetooth speakers image on the left is owned by Bang and Olufsen and is released under creative commons lisence. So, I downloaded it and displayed it here in my demo.</p>
+			<p class="button">Know More</p>
+		</div>
+</div>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.js"></script>
+<script src="https://unpkg.com/scrollreveal/dist/scrollreveal.min.js"></script>
+<script src="js/animation.js"></script>
+</body>
+</html>
---
+++ js/animation.js
@@ -0,0 +1,16 @@
+window.sr = ScrollReveal();
+	sr.reveal('header',{
+
+		duration:2000,
+		origin:'bottom'
+	});
+	sr.reveal('.images',{
+		duration:2000,
+		origin:'top',
+		distance:'300px'
+	});
+	sr.reveal('.heading',{
+		duration:2000,
+		origin:'right',
+		origin:'bottom'
+	});
